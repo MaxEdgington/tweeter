dd4e42e1c018b74571f2a46ababdc46881de2068
MaxEdgington <edgington.m.w@gmail.com>
2023-08-17 18:43:36 -0400

    pre-release, looking nearly ready for project completion

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,6 +1,111 @@
-/*
- * Client-side JS logic goes here
- * jQuery is already loaded
- * Reminder: Use (and do all your DOM work in) jQuery's document ready function
- */
+// Ready function
+$(() => {
+  loadTweets();
+  $("#tweetForm").on("submit", function(event) {
+    event.preventDefault();
 
+    //  serializing form data
+    const data = $(this).serialize();
+
+    //Ajax call
+    $.ajax({
+      url: "/tweets",
+      method: "POST",
+      data: data,
+      success: function(result) {
+        // Reversing error message if input provided after no input
+        $(".error-no-message").addClass("display-none");
+
+        // Emptying text area
+        $("#tweet-text").val("");
+        loadTweets();
+      },
+      error: function(error) {
+        console.log("there was an error ", error);
+
+        //Jquery sending error message if post does not work
+        $(".error-no-message").removeClass("display-none");
+      },
+    });
+  });
+});
+
+const createTweetElement = function(tweet) {
+  //Default entries for Tweet elements to handle incomplete data
+  let avatars = "https://i.imgur.com/73hZDYK.png";
+  let name = "No name";
+  let handle = "No handle";
+  let text = "No text";
+  let createdAt = "Undefined";
+  console.log("tweet object: ", tweet);
+
+  //Handling user data assignment
+  if (tweet.user.avatars) {
+    avatars = tweet.user.avatars;
+  }
+
+  if (tweet.user.name) {
+    name = tweet.user.name;
+  }
+
+  if (tweet.user.handle) {
+    handle = tweet.user.handle;
+  }
+
+  if (tweet.content.text) {
+    text = tweet.content.text;
+  }
+
+  if (tweet.created_at) {
+    createdAt = timeago.format(tweet.created_at);
+  }
+
+  // creating an escape function to prevent textarea script hacking
+  const escape = function(str) {
+    let div = document.createElement("div");
+    div.appendChild(document.createTextNode(str));
+    return div.innerHTML;
+  };
+
+  //Tweet template with template literals
+  const tweetMessage = `
+       <article>
+        <section class="tweet-header flex-justify">
+          <div class="avatar-name">
+            <img src = "${avatars}" class="avatar"/>
+            <h4>${name}</h4>
+          </div>
+          <h4>${handle}</h4>
+        </section>
+        <h2 class="tweeted-text">${escape(text)}</h2>
+        <hr>
+        <footer class="tweet-footer flex-justify">
+          <h4>${createdAt}</h4>
+          <div class="tweet-icons">
+            <i class="fa-solid fa-flag" ></i>
+            <i class="fa-solid fa-retweet"></i>
+            <i class="fa-solid fa-heart"></i>
+            
+          </div>
+        </footer>
+
+      </article>
+
+    `;
+  $(".main-container").prepend(tweetMessage);
+};
+
+const renderTweets = function(tweets) {
+  // clearing the DOM so duplicate entries are not visible
+  $(".main-container").empty();
+  for (let tweet of tweets) {
+    createTweetElement(tweet);
+  }
+};
+
+//Loads tweets with get, puts them all on page with render
+const loadTweets = () => {
+  $.get("/tweets", (data) => {
+    renderTweets(data);
+  });
+};
--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,37 +2,21 @@ $(document).ready(function() {
   console.log("working!");
   const characterLimit = 140;
 
-  
-  $("#tweet-text").on('input', function() {
-    // converting DOM this into Jquery object(this)
-    // all jquery objects have the same functions, all have val (although it might not do anything)
+  $("#tweet-text").on("input", function() {
+    // converting DOM this into Jquery object
+
     const input = $(this);
-    // console.log(input);
     const text = input.val();
     const form = input.parent();
     const counter = form.find(".counter");
     let counterVal = counter.val();
     $(counter).text(characterLimit - text.length);
-    
+
+    // Error if counter less than zero
     if (counterVal < 0) {
-      (counter.css("color", "red"));
+      $(".error-length").removeClass("display-none");
     } else {
-      (counter.css("color", "rgb(84, 81, 73)"));
+      $(".error-length").addClass("display-none");
     }
-    
-    console.log(counter.css("color"));
-    //console.log(text.length);
-    // console.log(text); //The this keyword is a reference to the button
   });
- 
-
-  /*
-  const tweetText = document.getElementById("tweet-text");
-  document.addEventListener("input", (event) =>{
-    console.log(event);
-    console.log(event.input);
-  });
-*/
-  
 });
-
